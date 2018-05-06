e50c298d760f74b18507c66825c9120e5286f3ba
aurinko <jollot@yandex.ru>
2018-05-06 21:43:37 +0700

    Удалила лишнее

--- app/src/main/java/com/oi/spaghet1/MainActivity.java
+++ app/src/main/java/com/oi/spaghet1/MainActivity.java
@@ -358,13 +358,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_favourites) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_help) {
 
         }
 
